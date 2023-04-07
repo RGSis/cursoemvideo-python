1efd0c6940dd773fa253d397cc39aabaf768f6b2
Raphael Gonçalves <raphael.itprofessional@gmail.com>
2023-04-07 10:29:38 -0300

    Exercício 02
    Mensagem de boas vindas e adição de linha em branco no final

---
+++ ex002.py
@@ -0,0 +1,6 @@
+# Exercício 002
+# Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas vindas.
+
+
+nome = input('Digite seu nome: ')
+print('Olá ' + nome + ' seja bem-vindo ao curso de Python 3!')
